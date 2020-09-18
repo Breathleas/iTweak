b73f9425d236c83cb4982777a32d2cade7387eab
jiaxw <jiaxw32@gmail.com>
2020-09-18 23:23:44 +0800

    微信粉丝数据抓取

--- Weibo/WeiboTweak.mm
+++ Weibo/WeiboTweak.mm
@@ -17,67 +17,106 @@
 #include <notify.h> // not required; for examples only
 #include "CYUtils.h"
 #include "AFNetWorkingCallback.h"
+#include "CYDatabaseManager.h"
+#include "WBUserFansModel.h"
+
+#define kWBUserFansTable @"wb_user_follower"
 
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wundeclared-selector"
 
-
-static NSMutableDictionary *g_dictFollowers;
-
-static NSMutableArray *g_followersID;
-
-static void parseFollowersData(id obj){
-    if (obj && [obj isKindOfClass:[NSDictionary class]]) {
-        NSArray *cards = obj[@"cards"];
-        if (cards && [cards isKindOfClass:[NSArray class]] && cards.count > 0) {
-            for (NSDictionary *item in cards) {
-                NSArray *cardGroup = item[@"card_group"];
-                for (NSDictionary *cardGroupItem in cardGroup) {
-                    NSDictionary *dictUser = cardGroupItem[@"user"];
-                    if (dictUser && [dictUser isKindOfClass:[NSDictionary class]]) {
-                        CHLog(@">>> %@", dictUser);
-                        NSString *userid = [dictUser[@"id"] description];
-                        if (userid && userid.length > 0) {
-                            if ([g_followersID containsObject:userid] == NO) {
-                                [g_dictFollowers setObject:dictUser forKey:userid];
-                                [g_followersID addObject:userid];
-                                CHLog(@">>> users count: %@", @(g_dictFollowers.count));
-                            } else {
-                                CHLog(@">>> user <%@> already exist.", userid);
-                            }
-                        }
+static void saveFollowersData(id obj){
+    if (!obj || [obj isKindOfClass:[NSDictionary class]] == NO) {
+        return;
+    }
+    
+    NSArray *cards = [(NSDictionary *)obj objectForKey:@"cards"];
+    if ([cards isKindOfClass:[NSArray class]] == NO || cards.count == 0) {
+        return;
+    }
+    
+    for (NSDictionary *item in cards) {
+        NSString *itemid = [item objectForKey:@"itemid"];
+        if (!itemid || itemid.length == 0) {
+            continue;
+        }
+        NSArray *cardGroup = item[@"card_group"];
+        for (NSDictionary *cardGroupItem in cardGroup) {
+            NSString *desc1 = [cardGroupItem objectForKey:@"desc1"];
+            NSString *desc2 = [cardGroupItem objectForKey:@"desc2"];
+            NSDictionary *dictUser = cardGroupItem[@"user"];
+            if (dictUser && [dictUser isKindOfClass:[NSDictionary class]]) {
+                WBUserFansModel *model = [WBUserFansModel yy_modelWithDictionary:dictUser];
+                model.rid = generateUUID();
+                model.itemid = itemid;
+                model.desc1 = desc1;
+                model.desc2 = desc2;
+                model.createdatetime = [[NSDate new] timeIntervalSince1970];
+                
+                
+                dispatch_async(dispatch_get_global_queue(0, 0), ^{
+                    NSString *existSql = [NSString stringWithFormat:@"select rid from %@ where itemid like ? and userid like ?", kWBUserFansTable];
+                    NSError *error;
+                    BOOL exist = [[CYDatabaseManager sharedDatabase] queryResultExist:existSql arrayArgs:@[model.itemid, model.userid] error:&error];
+                    if (error) {
+                        CHLog(@">>> execute sql failed: %@", error.description);
                     }
-                }
-            }
-        } else {
-            if (g_dictFollowers.count > 0) {
-                CHLog(@">>> save users, total count: %@", @(g_dictFollowers.count));
-                NSString *json = convertJsonObject2JsonString(g_dictFollowers.allValues);
-                saveStringToFile(json, @"Weibo", @"followers");
+                    
+                    if (exist) {
+                        CHLog(@">>> %@ already exist.", model);
+                        return;
+                    }
+                    
+                    
+                    NSString *insertSql = [NSString stringWithFormat:@"insert into %@(rid, itemid, userid, name, screen_name, profile_image_url, avatar_large, verified, verified_type, followers_count, friends_count, mbtype, mbrank, remark, desc1, desc2, createdatetime) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);", kWBUserFansTable];
+                    [[CYDatabaseManager sharedDatabase] saveDataWithSql:insertSql
+                                                              arrayArgs:@[
+                                                                  model.rid,
+                                                                  model.itemid ?: [NSNull null],
+                                                                  model.userid ?: [NSNull null],
+                                                                  model.name ?: @"",
+                                                                  model.screen_name ?: @"",
+                                                                  model.profile_image_url ?: @"",
+                                                                  model.avatar_large ?: @"",
+                                                                  @(model.verified),
+                                                                  @(model.verified_type),
+                                                                  @(model.followers_count),
+                                                                  @(model.friends_count),
+                                                                  @(model.mbtype),
+                                                                  @(model.mbrank),
+                                                                  model.remark ?: @"",
+                                                                  model.desc1 ?: @"",
+                                                                  model.desc2 ?: @"",
+                                                                  @(model.createdatetime)
+                                                              ]
+                                                               complete:^(BOOL success, NSError * _Nullable error) {
+                        if (success) {
+                            CHLog(@">>> insert %@ success.", model);
+                        } else {
+                            CHLog(@">>> insert %@ failed: %@", model, error.description);
+                        }
+                    }];
+                });
             }
         }
     }
 }
 
 CHConstructor{
-    g_dictFollowers = [NSMutableDictionary new];
-    g_followersID = [NSMutableArray new];
-    NSString *file = @"/var/mobile/users.json";
+    NSString *sql = [NSString stringWithFormat:@"CREATE TABLE IF NOT EXISTS %@(rid TEXT PRIMARY KEY, itemid TEXT, userid TEXT, name TEXT, screen_name TEXT, profile_image_url TEXT, avatar_large TEXT, verified INTEGER, verified_type INTEGER, followers_count INTEGER, friends_count INTEGER, mbtype INTEGER, mbrank INTEGER, remark TEXT, desc1 TEXT, desc2 TEXT, createdatetime real);", kWBUserFansTable];
     
-    NSData *data = [NSData dataWithContentsOfFile:file];
-    NSArray *followers = jsonObjectWithData(data);
-    if(followers && followers.count > 0){
-        for(NSDictionary *item in followers){
-            NSString *userid = item[@"userid"];
-            [g_followersID addObject:userid];
+    [[CYDatabaseManager sharedDatabase] createTableWithSql: sql complete:^(BOOL success, NSError * _Nullable error) {
+        if(success){
+            CHLog(@"create table %@ success.", kWBUserFansTable);
+        } else {
+            CHLog(@"create table %@ failed: %@", kWBUserFansTable, error.description);
         }
-        CHLog(@">>> users count: %@", @(g_followersID.count));
-    }
+    }];
     
     [AFNetWorkingCallback registerAFNSerializeResponseFinishedHandler:^(NSURLResponse * _Nonnull response, id  _Nonnull responseObject, NSError *__autoreleasing  _Nullable * _Nullable error) {
-        NSString *url = response.URL.absoluteString;
-        if([url containsString:@"/2/cardlist"]){//粉丝列表
-            parseFollowersData(responseObject);
+        NSString *url = response.URL.path;
+        if([url containsString:@"/2/cardlist"]){//粉丝列表接口
+            saveFollowersData(responseObject);
         } else if ([url containsString:@"/2/users/show"]){//用户信息
             
         }
