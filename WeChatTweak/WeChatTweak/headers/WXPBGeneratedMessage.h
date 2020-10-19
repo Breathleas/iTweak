887fd20321f5ba32f2ff493cde688428863d7010
jiaxw <jiaxw32@gmail.com>
2020-10-19 23:51:48 +0800

    add WeChat header files.

---
+++ WeChatTweak/WeChatTweak/headers/WXPBGeneratedMessage.h
@@ -0,0 +1,12 @@
+//
+//  Header.h
+//  WeCheckDylib
+//
+//  Created by shawnfee on 2020/4/16.
+//  Copyright © 2020 shawnfee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WXPBGeneratedMessage :NSObject
+@end
