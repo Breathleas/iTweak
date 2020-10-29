3ff405a8bb88e6432941be1d65cf91514ede8eaa
jiaxw <gis_som@163.com>
2020-10-29 23:33:43 +0800

    add daemon project

---
+++ idaemond/idaemond/main.mm
@@ -0,0 +1,92 @@
+//
+//  main.c
+//  idaemond
+//
+//  Created by jiaxw-mac on 2020/10/29.
+//  Copyright (c) 2020 ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#include <UIKit/UIKit.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <dlfcn.h>
+#include <notify.h>
+#include <stdlib.h>
+
+NSInteger hourOfDate(NSDate *date);
+
+NSInteger hourOfDate(NSDate *date){
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSUInteger unitFlags = NSCalendarUnitHour;
+    NSDateComponents *dateComponent = [calendar components:unitFlags fromDate:date];
+    return dateComponent.hour;
+}
+
+@interface CYAppManager : NSObject
+
+- (int)launchApp:(NSString *)identifier;
+
+- (void)timerFireHandler:(NSTimer *)timer;
+
+@end
+
+@implementation CYAppManager
+
+- (void)timerFireHandler:(NSTimer *)timer{
+    NSDate *date = [[NSDate alloc] init];
+    NSInteger hour = hourOfDate(date);
+    NSLog(@">>>> [daemonn]  current datetime: %f", date.timeIntervalSince1970);
+    static BOOL flag = true;
+    if (hour == 6 || hour == 23) {
+        if (flag) {
+            [self launchApp:@"com.tencent.xin"];
+            flag = false;
+        }
+    } else {
+        flag = true;
+    }
+}
+
+- (int)launchApp:(NSString *)bundleID{
+    void *sbServices = dlopen("/System/Library/PrivateFrameworks/SpringBoardServices.framework/SpringBoardServices", RTLD_LAZY);
+    if (!sbServices) {
+        /* fail to load the library */
+        NSLog(@">>>> [daemonn] dlopen error: %s", dlerror());
+        return -1;
+    }
+    
+    int (*SBSLaunchApplicationWithIdentifier)(CFStringRef identifier, Boolean suspended);
+    SBSLaunchApplicationWithIdentifier = (int(*)(CFStringRef,Boolean))dlsym(sbServices, "SBSLaunchApplicationWithIdentifier");
+    if (!SBSLaunchApplicationWithIdentifier) {
+        /* no such symbol */
+        NSLog(@">>>> [daemonn] dlsym error: %s", dlerror());
+        return -1;
+    }
+    
+    CFStringRef identifier = (__bridge CFStringRef)bundleID;
+    int r = SBSLaunchApplicationWithIdentifier(identifier, false);
+    dlclose(sbServices);
+    NSLog(@">>>> [daemonn] %s, %d", __FUNCTION__, r);
+    return r;
+}
+
+
+@end
+
+int main (int argc, const char * argv[])
+{
+    CYAppManager *manager = [[CYAppManager alloc] init];
+    NSTimer *timer = [[NSTimer alloc] initWithFireDate:[NSDate date]
+                                              interval: 10.0
+        target:manager
+        selector:@selector(timerFireHandler:)
+        userInfo:nil
+        repeats:YES];
+
+    NSRunLoop *runLoop = [NSRunLoop currentRunLoop];
+    [runLoop addTimer:timer forMode:NSDefaultRunLoopMode];
+    [runLoop run];
+    
+    return 0;
+}
+
